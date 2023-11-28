5d3c777231bb36a6f15880d3e422a1efe23d1b0a
Sirius B <root.siriusb@gmail.com>
2023-11-28 08:15:03 +0100

    stupid code alert

--- MarkSNDL.py
+++ MarkSNDL.py
@@ -5,31 +5,34 @@ def MarkSNDL(MDFile):
     with open(MDFile) as MarkDown_File:
         for line in MarkDown_File:
             FileLineArray.append(line)
-    for cycle in range (len(FileLineArray)):
+    for cycle in range (len(FileLineArray)): # This is dumb stupid code but it'll do for now.
         if FileLineArray[cycle][0:6] == "######":
             StringHotReplace = FileLineArray[cycle]
-            StringHotReplace.replace("######", "<h6>")
-            FileLineArray[cycle] = StringHotReplace + "</h6>"
+            StringHotReplace = StringHotReplace.replace("######", "<h6>") + StringHotReplace.replace("\n", "</h6>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
         elif FileLineArray[cycle][0:5] == "#####":
             StringHotReplace = FileLineArray[cycle]
-            FileLineArray[cycle].replace("#####", "<h5>")
-            FileLineArray[cycle] = FileLineArray[cycle] + "</h5>"
+            StringHotReplace = StringHotReplace.replace("#####", "<h5>") + StringHotReplace.replace("\n", "</h5>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
         elif FileLineArray[cycle][0:4] == "####":
             StringHotReplace = FileLineArray[cycle]
-            FileLineArray[cycle].replace("####", "<h4>")
-            FileLineArray[cycle] = FileLineArray[cycle] + "</h4>"
+            StringHotReplace = StringHotReplace.replace("####", "<h4>") + StringHotReplace.replace("\n", "</h4>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
         elif FileLineArray[cycle][0:3] == "###":
             StringHotReplace = FileLineArray[cycle]
-            FileLineArray[cycle].replace("###", "<h3>")
-            FileLineArray[cycle] = FileLineArray[cycle] + "</h3>"
+            StringHotReplace = StringHotReplace.replace("###", "<h3>") + StringHotReplace.replace("\n", "</h3>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
         elif FileLineArray[cycle][0:2] == "##":
             StringHotReplace = FileLineArray[cycle]
-            FileLineArray[cycle].replace("##", "<h2>")
-            FileLineArray[cycle] = FileLineArray[cycle] + "</h2>"
+            StringHotReplace = StringHotReplace.replace("##", "<h2>") + StringHotReplace.replace("\n", "</h2>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
         elif FileLineArray[cycle][0:1] == "#":
             StringHotReplace = FileLineArray[cycle]
-            StringHotReplace.replace("#", "<h1>")
-            FileLineArray[cycle] = StringHotReplace + "</h1>"
+            StringHotReplace = StringHotReplace.replace("#", "<h1>") + StringHotReplace.replace("\n", "</h1>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
+        else:
+            StringHotReplace = "<p>" + FileLineArray[cycle] + StringHotReplace.replace("\n", "</p>" + "\n")
+            FileLineArray[cycle] = StringHotReplace
             
     for cycle in range (len(FileLineArray)):
         print(FileLineArray[cycle])
